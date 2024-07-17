15ab9332c927ba0bdb8b4fb1f2075b6256debf52
POWERPAVEL05 <p.hoeft@mailbox.org>
2024-07-18 01:19:17 +0200

    Liste von erreichbaren Wlan-Netzwerken wird angezeigt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import curses
-import sys 
+import subprocess
+import sys
 import time
 '''
 def main(win):
@@ -27,17 +28,18 @@ curses.wrapper(main)
 
 stdscr = curses.initscr()
 menuItems = ["Start","Options","Help","Quit"]
-def menu(selected_row):
+def menu(selected_row,listItems):
     h, w = stdscr.getmaxyx()
     curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_WHITE)
-    for idx,item in enumerate(menuItems):
+    for idx,item in enumerate(listItems):
         x=w//2 - len(item)//2
-        y=h//2 - len(menuItems)+idx
+        #y=h//2 - len(listItems)+idx
+        y = 0 + idx
         if idx == selected_row:
-            setColor(1,item,y,x)
+            setColor(1,str(item),y,x)
         else:
             stdscr.addstr(y,x,item)
-    stdscr.refresh()
+    #stdscr.refresh()
 
 def main(stdscr):
     #cursor invisible
@@ -45,31 +47,45 @@ def main(stdscr):
     current_row_idx = 0
     #colorpair --> name,color-foreground, color-background
     #curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLUE)
-    while True:
-        menu(current_row_idx)
+    #stdscr.nodelay(True)
+    #timeTemp = time.time()
+    key = 0
+    itemsN = getSSID()
+    print(menuItems)
+    print(itemsN)
+    while True: 
+        menu(current_row_idx,itemsN)
+        #test = str(info)
+        #stdscr.addstr(0,0,test)
         key = stdscr.getch()
-        stdscr.addstr(0,0,str(key))
         stdscr.refresh()
         if key == 113:
             exit()
         elif key == 107:
             if current_row_idx == 0:
-                current_row_idx = len(menuItems)-1
+                current_row_idx = len(itemsN)-1
             else:
                 current_row_idx += -1
         elif key == 106:
-            if current_row_idx == len(menuItems)-1:
+            if current_row_idx == len(itemsN)-1:
                 current_row_idx = 0
             else:
                 current_row_idx += 1
+        stdscr.refresh()
         stdscr.clear()
-        stdscr.addstr(0,0,"Up")
+
+#gibt Liste von SSID wieder und Laenge der Liste
+def getSSID():
+    ssid = subprocess.run(["nmcli","-f" ,"SSID","-c","no","-t","d","w"],text=True,capture_output=True)
+    nameList = str(ssid.stdout).split()
+    return nameList
+
 
 def setColor(pairName,text,y,x):
     stdscr.attron(curses.color_pair(pairName))
     stdscr.addstr(y,x,text)
     stdscr.attroff(curses.color_pair(pairName))
-
+    
 if __name__ == "__main__":
     curses.wrapper(main)
 
