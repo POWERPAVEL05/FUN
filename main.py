2634325d7b4b885bb1eb58867f93457d1722bf24
POWERPAVEL05 <p.hoeft@mailbox.org>
2024-07-19 00:48:14 +0200

    Code cleanup, wlan-names and splash screen
    Feature:
    *'nmcli d w' subprocess in main() executed
    *program shows list of available wlan networks
    *SSID menu:max ammount of wlan connections shown can now be changed -->
    causes errors when list goes out of window bounds
    
    Cleaned up parts of the Code:
    *removed useless imports
    *defined method for color pairs
    *getSSID() requires win parameter
    *useless code removed from main()--> got rid of many refresh() calls
    
    Fixed:
    *getSSID() now actually displays full names instead of splitting them
    into multiple items

--- main.py
+++ main.py
@@ -1,7 +1,96 @@
 import curses
 import subprocess
-import sys
-import time
+
+menuItems = ["Start","Options","Help","Quit"]
+stdscr = curses.initscr()
+
+#Colors
+def initColors():
+    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_WHITE)
+#Takes list and displays vertical Menu, naviagate up and down through items, needs selected_row for cursor 
+#TODO: make more dynamic
+#TODO: account for multiple signals with same SSID
+def menuSSID(selected_row,listItems,win):
+    #h, w = win.getmaxyx()
+    try:
+        for idx,item in enumerate(listItems):
+            x= 1
+            y = 0 + idx
+            if idx > 15: break
+            if idx == selected_row:
+                setColor(1,str(item),y,x,win)
+            else:
+                win.addstr(y,x,item)
+    except:
+        pass #list to long -->  TODO: add error log?
+    win.refresh()
+
+#Everything is run from here:Initiating/Updating of windows, row and soon to come focus_idx etc.
+def main(stdscr):
+    #Init segment
+    initColors()
+    curses.curs_set(0)#cursor invisible
+    current_row_idx = 0
+    key = 0
+    #splash screen
+    stdscr.addstr(0,0,"FUN - is fetching data")
+    stdscr.refresh()
+    itemsN = getSSID(15)
+    win = curses.newwin(20,30,0,0)
+    win.refresh()
+    while True: 
+        #Draw order
+        menuSSID(current_row_idx,itemsN,win)
+        key = win.getch() 
+        #Keymanager
+        if key == ord("q"):
+            #incase of failure in other systems
+            curses.curs_set(1)
+            curses.nocbreak()
+            stdscr.keypad(False)
+            curses.echo()
+            exit()#curses.endwin() does not work, my fault?
+        elif key == ord("k"):
+            if current_row_idx == 0:
+                current_row_idx = len(itemsN)-1
+            else:
+                current_row_idx += -1
+        elif key == ord("j"):
+            if current_row_idx == len(itemsN)-1:
+                current_row_idx = 0
+            else:
+                current_row_idx += 1
+        #Clear
+        stdscr.clear()
+
+#gibt Liste von SSID wieder und Laenge der Liste
+#beakpoint specifies amount
+
+#TODO: handle 'I'm unkwown' and same routres emitting multiple singnals 
+def getSSID(breakpoint = None):
+    ssid = subprocess.run(["nmcli","-f" ,"SSID","-c","no","-t","d","w"],text=True,capture_output=True)
+    nameList = str(ssid.stdout).split("\n")
+    if breakpoint == None:
+        return nameList
+    else:
+        return nameList[:breakpoint]
+
+
+def setColor(pairName,text,y,x,win=stdscr):
+    #colorpair --> name,color-foreground, color-background
+    win.attron(curses.color_pair(pairName))
+    win.addstr(y,x,text)
+    win.attroff(curses.color_pair(pairName))  
+
+def init():
+    curses.curs_set(0)
+
+def updateAll():
+    pass
+
+if __name__ == "__main__":
+    curses.wrapper(main)
+
 '''
 def main(win):
     win.nodelay(True)
@@ -25,67 +114,3 @@ def main(win):
 
 curses.wrapper(main)
 '''
-
-stdscr = curses.initscr()
-menuItems = ["Start","Options","Help","Quit"]
-def menu(selected_row,listItems):
-    h, w = stdscr.getmaxyx()
-    curses.init_pair(1,curses.COLOR_BLACK,curses.COLOR_WHITE)
-    for idx,item in enumerate(listItems):
-        x=w//2 - len(item)//2
-        #y=h//2 - len(listItems)+idx
-        y = 0 + idx
-        if idx == selected_row:
-            setColor(1,str(item),y,x)
-        else:
-            stdscr.addstr(y,x,item)
-    #stdscr.refresh()
-
-def main(stdscr):
-    #cursor invisible
-    curses.curs_set(0)
-    current_row_idx = 0
-    #colorpair --> name,color-foreground, color-background
-    #curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLUE)
-    #stdscr.nodelay(True)
-    #timeTemp = time.time()
-    key = 0
-    itemsN = getSSID()
-    print(menuItems)
-    print(itemsN)
-    while True: 
-        menu(current_row_idx,itemsN)
-        #test = str(info)
-        #stdscr.addstr(0,0,test)
-        key = stdscr.getch()
-        stdscr.refresh()
-        if key == 113:
-            exit()
-        elif key == 107:
-            if current_row_idx == 0:
-                current_row_idx = len(itemsN)-1
-            else:
-                current_row_idx += -1
-        elif key == 106:
-            if current_row_idx == len(itemsN)-1:
-                current_row_idx = 0
-            else:
-                current_row_idx += 1
-        stdscr.refresh()
-        stdscr.clear()
-
-#gibt Liste von SSID wieder und Laenge der Liste
-def getSSID():
-    ssid = subprocess.run(["nmcli","-f" ,"SSID","-c","no","-t","d","w"],text=True,capture_output=True)
-    nameList = str(ssid.stdout).split()
-    return nameList
-
-
-def setColor(pairName,text,y,x):
-    stdscr.attron(curses.color_pair(pairName))
-    stdscr.addstr(y,x,text)
-    stdscr.attroff(curses.color_pair(pairName))
-    
-if __name__ == "__main__":
-    curses.wrapper(main)
-
